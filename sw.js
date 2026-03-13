466fbc217074967fddab33d519af57c13591afb3
ynkz96 <anes.zrno@gmail.com>
2026-03-13 16:04:14 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,41 @@
-self.addEventListener('install', (e) => {
-  console.log('HitMaster SW installed');
+const cacheName = 'hitmaster-v1';
+const staticAssets = ['./', './index.html', './manifest.json'];
+
+self.addEventListener('install', async e => {
+  const cache = await caches.open(cacheName);
+  await cache.addAll(staticAssets);
+  return self.skipWaiting();
 });
 
-self.addEventListener('fetch', (e) => {
-  // Prazan fetch omogućava rad u standalone modu
+self.addEventListener('activate', e => {
+  self.clients.claim();
 });
+
+self.addEventListener('fetch', async e => {
+  const req = e.request;
+  const url = new URL(req.url);
+
+  if (url.origin === location.origin) {
+    e.respondWith(cacheFirst(req));
+  } else {
+    e.respondWith(networkAndCache(req));
+  }
+});
+
+async function cacheFirst(req) {
+  const cache = await caches.open(cacheName);
+  const cached = await cache.match(req);
+  return cached || fetch(req);
+}
+
+async function networkAndCache(req) {
+  const cache = await caches.open(cacheName);
+  try {
+    const refresh = await fetch(req);
+    await cache.put(req, refresh.clone());
+    return refresh;
+  } catch (e) {
+    const cached = await cache.match(req);
+    return cached;
+  }
+}
